c5e7a15ead923696885def9fd150a7ce4049d4af
Nick Russo <nijaru7@gmail.com>
2025-12-23 12:33:39 -0800

    Fix jb v0.0.11 checksums after re-release

--- Formula/jb.rb
+++ Formula/jb.rb
@@ -8,12 +8,12 @@ class Jb < Formula
   on_macos do
     on_arm do
       url "https://github.com/nijaru/jb/releases/download/v0.0.11/jb-aarch64-apple-darwin.tar.gz"
-      sha256 "4199a0aa7c820010b2a687a53bb819c09fa819cb8fd7601cac21e8ffed34f301"
+      sha256 "6774ace92521669703a2711d3353c23dd3c016a82e4897154eb30dc59aef4d9a"
     end
 
     on_intel do
       url "https://github.com/nijaru/jb/archive/refs/tags/v0.0.11.tar.gz"
-      sha256 "9a432506a2de0808fddfe99361db935696948b7156634125e8a547e068636a7c"
+      sha256 "601e89f4dea5413be9260f3135fe9bc834f9cd5e3f7d9418185dcfd1f5f515be"
       depends_on "rust" => :build
     end
   end
@@ -21,12 +21,12 @@ class Jb < Formula
   on_linux do
     on_arm do
       url "https://github.com/nijaru/jb/releases/download/v0.0.11/jb-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "3c212cabe9c6b3157c14e7dc19c82a888ebf640c06c999bbdd3d6752a575262d"
+      sha256 "30da0550bf59f59262460399bfb587d09047e05ec8771090b1bbcf6bfba57de9"
     end
 
     on_intel do
       url "https://github.com/nijaru/jb/releases/download/v0.0.11/jb-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "dc059b004c82223765735b02711cd94826d646908ce7ddbe6a9fe1173b3daafa"
+      sha256 "3999ad2d52ae60864c9e6de216499b079fe89d94b7baf4bb74148688e7be3599"
     end
   end
 
